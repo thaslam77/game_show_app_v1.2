8228a2c48f37e93535bb8c3fad82bed6b1a7a07d
Tony Haslam <thaslam77@gmail.com>
2020-09-23 14:54:06 -0400

    Code Clean up
    Code Clean up

--- js/app.js
+++ js/app.js
@@ -6,23 +6,21 @@ let gameButton = document.querySelector('.btn__reset');
 let keyrowBtn = document.querySelectorAll('.keyrow button');
 let missed = 0;
 let phrases = [
-    "IT WAS ALL A DREAM",
-    "IF I WAS A BILLIONARE",
-    "EVERYDAY PEOPLE",
-    "JUST DO IT",
+    "FOCUS ON GOALS NOT OBSTACLES",
+    "THOUGHTS BECOME THINGS",
+    "LITTLE THINGS MAKE BIG DAYS",
+    "SOMETIMES LATER BECOMES NEVER ",
     "DREAMS WEIGH MORE THAN EXCUSES",
-    'KILLING ME SOFTLY'
+    'HARD DOES NOT MEAN IMPOSSIBLE'
 ];
+let phraseArray = getRandomPhraseAsArray(phrases);
 let match = ''; 
 
 gameButton.addEventListener('click', () => {
     gameButton.style.display = "none";    
     overLayDiv.style.display = 'none';
-
     if (gameButton.textContent === 'Try Again!') {
         newGame();
-        // phrase.removeChild(ul);
-        // document.location.reload();
     }
 });
 
@@ -30,7 +28,6 @@ gameButton.addEventListener('click', () => {
 function getRandomPhraseAsArray(arr) {
     let randomNumber = Math.floor( Math.random() * arr.length);
     let phraseIndex = phrases[randomNumber];
-    //console.log(`Inside loop to get ${phraseIndex}`) 
     return phraseIndex;
 }
 
@@ -42,7 +39,7 @@ function addPhraseToDisplay(randomArr) {
             let li = document.createElement('li');
             li.textContent = randomPhrase[i];
                 if (li.textContent !== " ") {
-                    li.className = 'letter';
+                    li.className = 'letter';                    
                 } else {
                     li.className = 'space';
                 }
@@ -52,7 +49,6 @@ function addPhraseToDisplay(randomArr) {
 
 function checkLetter(guess) {    
     let btnClicked = guess.toUpperCase();
-    console.log(`button clicked: ${btnClicked}`);
     let phraseList = document.querySelectorAll('li.letter');
     let heartLI = document.querySelectorAll('li.tries');   
     let scoreboardList = document.querySelector('#scoreboard ol');   
@@ -60,18 +56,13 @@ function checkLetter(guess) {
    
         for ( let i = 0; i < phraseList.length; i++ ) {
             if (btnClicked === phraseList[i].textContent) {                
-                phraseList[i].className = 'show';   
-                match = btnClicked;                
-                console.log(`matched letter: ${match}`);         
+                phraseList[i].className = 'show swing';   
+                match = btnClicked;      
             } 
-        }        
-        
-        console.log(`Out of loop ${match}`);       
-        
-        if ( match === null && heartLI.length > 0) {
+        }
+            if ( match === null && heartLI.length > 0) {
             scoreboardList.removeChild(heartLI[0]);
-            missed++;
-            console.log(`Missed count: ${missed}`);            
+            missed++;          
         }
         return match; 
 }                    
@@ -80,27 +71,20 @@ function checkWin() {
     let phraseLetters = document.querySelectorAll('li.letter');
     let phraseShow = document.querySelectorAll('li.show');
     let randomPhrase = phraseLetters.length + phraseShow.length;
-    console.log(`Phrase letter count: ${randomPhrase}`);
-    console.log(`Phrase Letters: ${phraseLetters.length} and Phrase Show Class: ${phraseShow.length}`)
     
         if (randomPhrase === phraseShow.length) {
-            console.log('You win');
             overLayDiv.style.display = '';
             overLayDiv.className = "win";   
             gameButton.style.display = "inline-block"; 
             gameButton.textContent = "Try Again!" 
-            document.getElementById("status").innerHTML = "You Win!";  
-            // newGame();
+            document.getElementById("status").innerHTML = "You Win!";              
         }
-
-        if (missed === 5) {
-            console.log('You lose');           
+        if (missed === 5) {       
             overLayDiv.style.display = '';
             overLayDiv.className = "lose";   
             gameButton.style.display = "inline-block"; 
             gameButton.textContent = "Try Again!";            
-            document.getElementById("status").innerHTML = "You Lose!";  
-            // newGame();
+            document.getElementById("status").innerHTML = "You Lose!";              
         }
 }
 
@@ -115,51 +99,36 @@ function newGame() {
     for (let i = 0; i < phraseLetters.length; i++ ){
         ul.removeChild(phraseLetters[i]);        
     }
-
     for (let i = 0; i < phraseSpaces.length; i++ ){
         ul.removeChild(phraseSpaces[i]);
     }
-
     for (let i = 0; i < phraseShow.length; i++ ){
         ul.removeChild(phraseShow[i]);
     }
-
     for (let i = 0; i < keyrowBtn.length; i++ ){
-        console.log(`Keyrows: ${keyrowBtn.length}`);
         if (keyrowBtn[i].tagName === 'BUTTON') {
             keyrowBtn[i].className = '';
             keyrowBtn[i].disabled = false;
         }
     }
-
     for (let i = 0; i < missed; i++ ){        
         scoreboardList.innerHTML += '<li class="tries"><img src="images/liveHeart.png" height="35px" width="30px"></li>';     
     }
-    missed = 0;
     
+    missed = 0;    
     phraseArray = getRandomPhraseAsArray(phrases);
     addPhraseToDisplay(phraseArray);
-    // console.log(`${phraseArray}`);
-
-    // document.location.reload();
 }
-
-let phraseArray = getRandomPhraseAsArray(phrases);
-addPhraseToDisplay(phraseArray);
-console.log(`${phraseArray}`); 
-
 
 qwerty.addEventListener('click', (event) => {
     let letterFound = '';
-    console.log(letterFound); 
     if (event.target.tagName === 'BUTTON') {
         event.target.className = 'chosen';
         event.target.disabled = true;
         letterFound = event.target.textContent;
-        checkLetter(letterFound);        
-
-    }   console.log(`letter returned: ${letterFound}`); 
-
-    checkWin();
-    
+        checkLetter(letterFound);
+    }   
+    checkWin();    
 });
+
+addPhraseToDisplay(phraseArray);
